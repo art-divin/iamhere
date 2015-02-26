1cb7739e61e17d4ca5c8eb911aae1ae8c6d9f54e
Ruslan Alikhamov <r.alikhamov@gmail.com>
2015-02-26 02:03:21 +0100

    bootstrapped place model

---
+++ iAmHERE/iAmHERE/model/place/IAHPlace.h
@@ -0,0 +1,13 @@
+//
+//  IAHPlace.h
+//  iAmHERE
+//
+//  Created by Ruslan Alikhamov on 26/02/15.
+//  Copyright (c) 2015 Ruslan Alikhamov. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface IAHPlace : NSManagedObject
+
+@end
---
+++ iAmHERE/iAmHERE/model/place/IAHPlace.m
@@ -0,0 +1,13 @@
+//
+//  IAHPlace.m
+//  iAmHERE
+//
+//  Created by Ruslan Alikhamov on 26/02/15.
+//  Copyright (c) 2015 Ruslan Alikhamov. All rights reserved.
+//
+
+#import "IAHPlace.h"
+
+@implementation IAHPlace
+
+@end
