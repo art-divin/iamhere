458262e5076965f5de09995ea0992ac62c298fad
Ruslan Alikhamov <r.alikhamov@gmail.com>
2015-02-27 01:58:32 +0100

    implemented assets manager

---
+++ iAmHERE/iAmHERE/ui/assets/IAHAssetsManager.h
@@ -0,0 +1,17 @@
+//
+//  IAHAssetsManager.h
+//  iAmHERE
+//
+//  Created by Ruslan Alikhamov on 27/02/15.
+//  Copyright (c) 2015 Ruslan Alikhamov. All rights reserved.
+//
+
+@import UIKit;
+
+@interface IAHAssetsManager : NSObject
+
++ (UIImage *)imageForTabBarMap;
++ (UIImage *)imageForTabBarSearch;
++ (UIImage *)imageForTabBarItinerary;
+
+@end
